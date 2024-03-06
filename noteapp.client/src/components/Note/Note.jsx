751d505aa0f8d5e278bc79c77b928ca699706cf3
Lukasz Welna <lukasz.welna96@gmail.com>
2024-03-06 20:33:21 +0100

    Added possibility to edit notes

--- noteapp.client/src/components/Note/Note.jsx
+++ noteapp.client/src/components/Note/Note.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './Note.css';
 import PropTypes from 'prop-types';
 import {
@@ -10,30 +10,82 @@ import {
 } from 'mdb-react-ui-kit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
-
+import { useClickAway } from '@uidotdev/usehooks';
+import DoneIcon from '@mui/icons-material/Done';
 
 function Note(props) {
 
-    const handleDelete = () => {
+    // useState hooks
+    const [title, setTitle] = useState(props.title);
+    const [content, setContent] = useState(props.content);
+    const [modify, setModify] = useState(false);
+
+    const ref = useClickAway(() => {
+        setModify(false);
+        setTitle(props.title);
+        setContent(props.content);
+    });
+
+    // Handlers
+    const handleModify = (event) => {
+        event.preventDefault();
+        setModify(true);
+    }
+
+    const handleDelete = (event) => {
+        event.preventDefault();
         props.handleDelete(props.id);
+    }
+
+    const handleEdit = (event) => {
+        event.preventDefault();
+        setModify(false);
+        props.handleEdit(props.id, title, content)
+    }
+
+    // CSS properties
+    const modifyStyle = {
+        backgroundColor: '#FFFDE7'
+    }
+
+    const normalStyle = {
+        backgroundColor: 'white'
     }
 
     return (
         <MDBCol className='px-2'>
-            <MDBCard className='h-100'>
+            <MDBCard className='h-100' style={modify ? modifyStyle : normalStyle}>
                 <MDBCardBody className='p-3'>
-                <form className='form-note'>
+                    <form className='form-note' ref={ref} onSubmit={handleEdit}>
                     <MDBCardTitle>
-                            {props.title }
+                            <input placeholder='Title' id='form-modify-title' 
+                                onChange={(e) => setTitle(e.target.value)} value={title}
+                                maxLength='50' autoComplete='off' required disabled={modify ? false : true}
+                                style={modify ? modifyStyle : normalStyle} />
                         </MDBCardTitle>
                         <MDBCardText className='mb-4'>
-                            {props.content }
+                            <textarea placeholder='Content' id='form-modify-content' rows='3'
+                                onChange={(e) => setContent(e.target.value)} value={content}
+                                maxLength='400' disabled={modify ? false : true}
+                                style={modify ? modifyStyle : normalStyle} />
                         </MDBCardText>
                         <MDBCol className='d-flex justify-content-end note-icons'>
-                            <button className='button-edit me-2 p-0'><EditIcon /></button>
-                            <button className='button-delete p-0' type='button' onClick={handleDelete}><DeleteIcon /></button>
+                            {!modify ?
+                                <div>
+                                    <button className='button-edit me-2 p-0' type='button' onClick={handleModify}>
+                                        <EditIcon />
+                                    </button>
+
+                                    <button className='button-delete p-0' type='button' onClick={handleDelete}>
+                                        <DeleteIcon />
+                                    </button>
+                                </div>
+                                :
+                                <button className='button-done p-0'>
+                                    <DoneIcon fontSize='large' />
+                                </button> }
                         </MDBCol>
-                 </form>        
+                    </form>      
                 </MDBCardBody>
             </MDBCard>
         </MDBCol> 
@@ -44,7 +96,8 @@ Note.propTypes = {
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     content: PropTypes.string.isRequired,
-    handleDelete: PropTypes.func.isRequired
+    handleDelete: PropTypes.func.isRequired,
+    handleEdit: PropTypes.func.isRequired
 };
 
 export default Note;
